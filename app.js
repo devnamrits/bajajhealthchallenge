50355d25eae8aaa2a35e3ca347344e68bc72dafa
Hritik Kumar <hrk10321@gmail.com>
2021-08-03 11:43:58 +0530

    Added app

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = express();
 
 const PORT = 3000;
 
+const logicRoute = require('./Routes/logic');
+
+app.use(express.json({ extended: false }))
+
+app.use(logicRoute)
+
+app.use('/', (req, res) => {
+    res.send('Redirected');
+})
+
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 })
